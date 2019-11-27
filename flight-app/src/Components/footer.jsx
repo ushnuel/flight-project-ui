b2d155c768873e25dbc1b6f80e73efb8daf3f44e
Emmanuel <ezeemmanuel2010@gmail.com>
2019-11-27 09:56:45 +0100

    Updates

--- flight-app/src/Components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const footer = () => (
-  <div className='bk-footer'>
-    <p>&copy; 2019 Flight Search. All rights reserved</p>
-  </div>
-);
-
-export default footer;
